5ab53f3a017e3be253a7de6c551f3a4aff161e69
tako_desktop <takotako@gmail.com>
2019-02-15 09:20:39 +0100

    imported the coco dataset from ms repo

---
+++ get_coco_detect_human_data.py
@@ -0,0 +1,7 @@
+from motion.detect_human import COCODataset
+
+def main():
+    
+
+if __name__ == "__main__":
+    main()
--- motion/config.py
+++ motion/config.py
@@ -1,6 +1,13 @@
 resize = {"height": 256, "width": 192}
 
 
+def get_image_size(key=None):
+    if key is None:
+        return resize
+    else:
+        return resize
+
+
 def output_shape(key):
     if resize["height"] == 256 and resize["width"] == 192:
         if key == "height":

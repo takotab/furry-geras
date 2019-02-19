26d40d89a7cd9c410d52d06f5ff55d0afed3f0aa
tako_desktop <takotako@gmail.com>
2019-02-19 13:47:26 +0100

    made a nice data set

--- motion/detect_human/bbox_dataset.py
+++ motion/detect_human/bbox_dataset.py
@@ -5,7 +5,7 @@ import os
 from pathlib import Path
 import pandas as pd
 from torch.utils.data import Dataset
-from skimage import io
+from PIL import Image
 import numpy as np
 
 from motion import config
@@ -14,24 +14,44 @@ logger = logging.getLogger(__name__)
 
 
 class BBoxDataset(Dataset):
-    def __init__(self, csv_file, transform=None):
+    def __init__(self, csv_file, size=500, transform=None, type="train"):
         super(BBoxDataset).__init__()
         self.df = pd.read_csv(csv_file)
         self.transform = transform
+        self._size = size
+        self.type = type
 
     def __len__(self):
         return self.df.shape[0]
 
     def __getitem__(self, index):
         data = dict(self.df.iloc[index, :])
-        sample = {
-            "image": io.imread(data["filename"]),
-            "landmarks": np.array(
-                [data["y"], data["x"], data["height"], data["width"]]
-            ),
-        }
+        img = Image.open(data["filename"])
+        crop, bbox = self._make_crop_sizes(data)
+        img = img.crop(crop)
+        sample = {"image": img, "bboxes": [bbox], "category_id": [0]}
 
         if self.transform:
             sample = self.transform(sample)
+
         return sample
+
+    def _make_crop_sizes(self, dct, train=True):
+        x, y, w, h = list(dct["bbox"])
+        x_crop, x_bbox = self._select_size_one_dim(x, w)
+        y_crop, y_bbox = self._select_size_one_dim(y, h)
+        bbox = [x_bbox[0], y_bbox[0], x_bbox[1], y_bbox[1]]
+        crop = [x_crop[0], y_crop[0], x_crop[1], y_crop[1]]
+        return crop, bbox
+
+    def _select_size_one_dim(self, start, length):
+        if self.type == "train":
+            center = np.random.randint(start, start + length + 1)
+        else:
+            center = np.mean([start, start + length + 1])
+        crop = [int(center - self._size / 2), int(center + self._size / 2)]
+        if start + length > crop[1]:
+            length = crop[1] - start
+        bbox = [start - crop[0], start - crop[0] + length]
+        return crop, bbox
 

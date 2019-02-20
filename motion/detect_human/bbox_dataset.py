02d79a77571976ccd1b5132ed9e73a0965d3c4b7
tako_desktop <takotako@gmail.com>
2019-02-20 13:29:47 +0100

    finally have a working resize algo. tested.

--- motion/detect_human/bbox_dataset.py
+++ motion/detect_human/bbox_dataset.py
@@ -7,6 +7,8 @@ import pandas as pd
 from torch.utils.data import Dataset
 from PIL import Image
 import numpy as np
+import cv2
+from ast import literal_eval
 
 from motion import config
 
@@ -16,9 +18,9 @@ logger = logging.getLogger(__name__)
 class BBoxDataset(Dataset):
     def __init__(self, csv_file, size=500, transform=None, type="train"):
         super(BBoxDataset).__init__()
-        self.df = pd.read_csv(csv_file)
+        self.df = pd.read_csv(csv_file, converters={"bbox": literal_eval})
         self.transform = transform
-        self._size = size
+        self._size = int(size)
         self.type = type
 
     def __len__(self):
@@ -26,32 +28,61 @@ class BBoxDataset(Dataset):
 
     def __getitem__(self, index):
         data = dict(self.df.iloc[index, :])
-        img = Image.open(data["filename"])
-        crop, bbox = self._make_crop_sizes(data)
-        img = img.crop(crop)
-        sample = {"image": img, "bboxes": [bbox], "category_id": [0]}
+        x, y, bbox = self._make_crop_sizes(data)
+
+        img = cv2.imread(data["filename"])
+        n_img = np.random.randint(0, 255, size=(self._size, self._size, 3)).astype(
+            np.uint8
+        )
+        n_img[y[0] : y[1], x[0] : x[1], :] = img[y[2] : y[3], x[2] : x[3], :]
+        sample = {"image": np.array(n_img), "bboxes": bbox, "category_id": [0]}
 
         if self.transform:
             sample = self.transform(sample)
 
         return sample
 
-    def _make_crop_sizes(self, dct, train=True):
-        x, y, w, h = list(dct["bbox"])
-        x_crop, x_bbox = self._select_size_one_dim(x, w)
-        y_crop, y_bbox = self._select_size_one_dim(y, h)
-        bbox = [x_bbox[0], y_bbox[0], x_bbox[1], y_bbox[1]]
-        crop = [x_crop[0], y_crop[0], x_crop[1], y_crop[1]]
-        return crop, bbox
+    def _make_crop_sizes(self, dct):
+        x, y, w, h = dct["bbox"]
+        x, x_bbox = self._resize_one_dims(x, w, dct["width"])
+        y, y_bbox = self._resize_one_dims(y, h, dct["height"])
 
-    def _select_size_one_dim(self, start, length):
+        bbox = [x_bbox[0], y_bbox[0], x_bbox[1] - x_bbox[0], y_bbox[1] - y_bbox[0]]
+
+        return x, y, bbox
+
+    def _resize_one_dims(self, start, length, orign_len):
+        start, length, orign_len, size = (
+            int(start),
+            int(length),
+            int(orign_len),
+            self._size,
+        )
         if self.type == "train":
-            center = np.random.randint(start, start + length + 1)
+            pick_start = np.random.randint
         else:
-            center = np.mean([start, start + length + 1])
-        crop = [int(center - self._size / 2), int(center + self._size / 2)]
-        if start + length > crop[1]:
-            length = crop[1] - start
-        bbox = [start - crop[0], start - crop[0] + length]
-        return crop, bbox
+            pick_start = lambda o, u: np.mean([o, u])
+        bbox = None
+        if orign_len > size:
+            if length > size:
+                m = start + length / 2
+                o_s = np.min([int(m - (size / 2)), orign_len - size])
+                o_e = o_s + size
 
+                n_s = 0
+                n_e = size
+                bbox = [0, size]
+            else:
+                s = np.min([start, orign_len - size])
+                o_s = int(pick_start(0, s))
+                o_e = size + o_s
+                n_s = 0
+                n_e = size
+        else:
+            o_s = 0
+            o_e = orign_len
+            n_s = int(pick_start(0, size - orign_len))
+            n_e = n_s + orign_len
+        if bbox is None:
+            bbox = [start - o_s + n_s, start + length - o_s + n_s]
+        return [n_s, n_e, o_s, o_e], bbox
--- test/test_BBoxdataset.py
+++ test/test_BBoxdataset.py
@@ -1,15 +1,44 @@
-from torchvision import transforms  # datasets, models,
+import numpy as np
 from motion.detect_human import BBoxDataset
 
 
 def test_bbox_dataset():
-    transform = transforms.Compose(
-        [
-            transforms.RandomResizedCrop(224),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-        ]
-    )
-    bbox_data = BBoxDataset("coco/one_human.csv", transform=transform)
-    print(bbox_data[0])
+    size = 500
+    bbox_data = BBoxDataset("coco/val2017_one_human.csv", size=size)
+    sample = bbox_data[0]
+    assert sample["image"].shape == (size, size, 3)
+    assert ["image", "bboxes", "category_id"] == list(sample.keys())
+
+
+def test_one_dim():
+    bbox_ds = BBoxDataset("coco/val2017_one_human.csv", type="valid")
+
+    def test_fn(fn, start, length, orign_len, size, **kwags):
+        # plt.figure()
+        or_img = np.ones(orign_len)
+        or_img[start : start + length] = 2
+        n_img = np.random.rand(size)
+        # plt.plot(or_img)
+        # plt.plot(n_img)
+        # plt.figure()
+        x, bbox = fn(start, length, orign_len)
+        # print(x)
+        n_img[x[0] : x[1]] = or_img[x[2] : x[3]]
+        # plt.plot(n_img)
+        # plt.plot([bbox[0], bbox[1]], [1, 1])
+        # plt.title(str(x) + str(bbox))
+
+    start, length, orign_len, size = 50, 550, 650, 500
+    test_fn(bbox_ds._resize_one_dims, start, length, orign_len, size)
+
+    start, length, orign_len, size = 50, 151, 651, 501
+    test_fn(bbox_ds._resize_one_dims, start, length, orign_len, size)
+
+    start, length, orign_len, size = 50, 150, 400, 500
+    test_fn(bbox_ds._resize_one_dims, start, length, orign_len, size)
+
+    start, length, orign_len, size = 344, 127, 640, 500
+    test_fn(bbox_ds._resize_one_dims, start, length, orign_len, size)
+    start, length, orign_len, size = 100, 501, 640, 500
+    test_fn(bbox_ds._resize_one_dims, start, length, orign_len, size)
+
